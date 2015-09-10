b7b51cb84f7a384cee428770b17f797f17297f52
Zhennan Wang <zhennan.wang@me.com>
2015-09-10 18:43:27 -0400

    Palindrome Number

---
+++ isPalindrome.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+        	return False
+        div = 1
+
+        while x/div >= 10:
+        	div *= 10
+
+        while x != 0:
+        	l = x // div
+        	r = x % 10
+        	if l != r:
+        		return False
+        	else:
+        		x = x % div // 10
+        		div /= 100
+        return True 
+
