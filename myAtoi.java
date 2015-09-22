edb5b95101b04f1c3ab1209b8743354cc8581a59
Zhennan Wang <zhennan.wang@me.com>
2015-09-21 22:05:35 -0400

    atoi java

---
+++ myAtoi.java
@@ -0,0 +1,24 @@
+public class Solution {
+	private static final int upperBound = Integer.MAX_VALUE / 10;
+    public int myAtoi(String str) {
+    	str = str.trim();
+    	int i = 0, n = str.length()-1;
+    	int sign = 1;
+    	if (i < n && str.charAt(i) == '+') {
+    		i++;
+    	} else if (i < n && str.charAt(i) == '-') {
+    		i++;
+    		sign = -1;
+    	}
+    	int num = 0;
+    	while (i < n && Character.isDigit(str.charAt(i))) {
+    		int digit = Character.getNumericValue(str.charAt(i));
+    		if (num > upperBound || num == upperBound && digit >= 8) {
+				return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;    			
+    		}
+    		num = num * 10 + digit;
+    		i++;
+    	}
+    	return sign * num;
+    }
+}
