3aa496cc4d30efbb796c89cf54df452707b9b4c1
Zhennan Wang <zhennan.wang@me.com>
2015-09-21 22:43:17 -0400

    valid number with regex

---
+++ validNumber.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isNumber(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        import re
+        return bool(re.match("^\s*[\+-]?((\d+(\.\d*)?)|\.\d+)([eE][\+-]?\d+)?\s*$", s))
+        
