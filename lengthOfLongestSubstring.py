3e4a3d2983f4988363ee89bb125d2793c0e52eda
Zhennan Wang <zhennan.wang@me.com>
2015-09-22 00:55:22 -0400

    longest substring 1 pass

---
+++ lengthOfLongestSubstring.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        # store index of character 
+        charMap = [-1] * 256
+        i, maxLen = 0, 0
+        for x in xrange(0, len(s)):
+        	if charMap[ord(s[x])] >= i:
+        		i = charMap[ord(s[x])] + 1
+        	charMap[ord(s[x])] = x
+        	maxLen = max(x-i+1, maxLen)
+        return maxLen	
