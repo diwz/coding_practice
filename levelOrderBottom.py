e8f08617228bbdefe0619b09a01623aa54c799e1
Zhennan Wang <zhennan.wang@me.com>
2015-09-25 10:50:29 -0400

    level order

---
+++ levelOrderBottom.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution(object):
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        res = []
+        self.preOrder(root, 0, res)
+        res = res[::-1]
+        return res
+
+    def preOrder(self, root, level, res):
+        if root:
+            if len(res) < level + 1:
+                res.append([])
+            res[level].append(root.val)
+            self.preOrder(root.left, level+1, res)
+            self.preOrder(root.right, level+1, res)
