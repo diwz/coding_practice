80e18bffec2b99e4f44806c4362356a39ee9edb3
Zhennan Wang <zhennan.wang@me.com>
2015-10-15 17:23:40 -0400

    merge two sorted lists

---
+++ MergeTwoSortedLinkedList.py
@@ -0,0 +1,54 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class recursiveSolution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        # Recursive solution
+        if l1 is None:
+            return l2
+        if l2 is None:
+            return l1
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+
+
+class iterativeSolution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if l1 is None:
+            return l2
+        if l2 is None:
+            return l1
+        dummy = ListNode(-1)
+        tmp = dummy
+        while l1 and l2:
+            if l1.val < l2.val:
+                tmp.next = l1
+                l1 = l1.next
+                tmp = tmp.next
+            else:
+                tmp.next = l2
+                l2 = l2.next
+                tmp = tmp.next
+        if not l1:
+            tmp.next = l2
+        else:
+            tmp.next = l1
+        return dummy.next
