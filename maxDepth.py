23eea0d97f73b5d430544eba25edb10498e56b25
Zhennan Wang <zhennan.wang@me.com>
2015-10-29 14:47:34 -0400

    add iterative sol for max depth of binary tree

--- maxDepth.py
+++ maxDepth.py
@@ -4,9 +4,9 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
+from collections import deque
 
-
-class Solution(object):
+class Solution1(object):
     def maxDepth(self, root):
         """
         :type root: TreeNode
@@ -15,3 +15,31 @@ class Solution(object):
         if root is None:
             return 0
         return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+
+
+class Solution2(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root is None:
+            return 0
+        queue = deque()
+        levels = deque()
+
+        queue.appendleft(root)
+        levels.appendleft(1)
+
+        while queue:
+            node = queue.pop()
+            level = levels.pop()
+            if node.left:
+                queue.appendleft(node.left)
+                levels.appendleft(level + 1)
+            if node.right:
+                queue.appendleft(node.right)
+                levels.appendleft(level + 1)
+            if not queue:
+                return level
+        return 0
