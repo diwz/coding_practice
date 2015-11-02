c3e9d14076a38e660ab6838fcf88a8a0601bd907
Zhennan Wang <zhennan.wang@me.com>
2015-11-01 23:18:22 -0500

    remove carry flag

--- plusOne.py
+++ plusOne.py
@@ -9,15 +9,11 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        carry = 0
         for i in range(len(digits)-1, -1, -1):
             if digits[i] == 9:
                 digits[i] = 0
-                carry = 1
             else:
                 digits[i] += 1
-                carry = 0
-                break
-        if carry:
-            digits.insert(0, 1)
+                return digits
+        digits.insert(0, 1)
         return digits
