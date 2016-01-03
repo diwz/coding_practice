70a19fe90edc8cf42a305da1db2f9e6a2ceacb39
Zhennan Wang <zhennan.wang@me.com>
2016-01-03 15:42:26 -0800

    rename file

--- insert_interval.py
+++ insert_interval.py
@@ -1,5 +1,4 @@
 # Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
-
 # You may assume that the intervals were initially sorted according to their start times.
 
 # Example 1:
@@ -7,7 +6,6 @@
 
 # Example 2:
 # Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] in as [1,2],[3,10],[12,16].
-
 # This is because the new interval [4,9] overlaps with [3,5],[6,7],[8,10].
 
 # Definition for an interval.
