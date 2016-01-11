ea080429950c41c19ef88a3410468b91d43d3209
Zhennan Wang <zhennan.wang@me.com>
2016-01-10 20:55:19 -0500

    power of three

--- power_of_three.py
+++ power_of_three.py
@@ -4,10 +4,18 @@
 # Could you do it without using any loop / recursion?
 import math
 
-class Solution(object):
+class Solution1(object):
     def isPowerOfThree(self, n):
         """
         :type n: int
         :rtype: bool
         """
-        return n > 0 and abs(math.log(n, 3) - round(math.log(n,3))) < 1e-10
+        return n > 0 and abs(math.log(n, 3) - round(math.log(n, 3))) < 1e-10
+
+class Solution2(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return False if n <= 0 else n == round(3 ** round(math.log(n, 3)))
