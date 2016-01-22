a7961f3d01608c09807b5064465e7c0e42da3de1
Zhennan Wang <zhennan.wang@me.com>
2016-01-22 18:14:33 -0500

    tab to space

--- twoSum.py
+++ twoSum.py
@@ -1,14 +1,14 @@
 class Solution(object):
-	"""docstring for Solution"""
-	def twoSum(self, nums, target):
-		"""
-		:type nums: List[int]
-		:type target: int
-		:rtype: List[int]
-		"""
-		map = {}
-		for idx, val in enumerate(nums, start = 1):
-			if (target - val) not in map:
-				map[val] = idx
-			else:
-				return (idx, map[target-val]) if idx < map[target-val] else (map[target-val], idx)
+    """docstring for Solution"""
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        map = {}
+        for idx, val in enumerate(nums, start=1):
+            if (target - val) not in map:
+                map[val] = idx
+            else:
+                return (idx, map[target-val]) if idx < map[target-val] else (map[target-val], idx)
